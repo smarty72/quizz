be99ea8e300a25b83366b96e9454cfc42bc77ccc
Martin <martin@abcrm.nl>
2024-01-12 08:39:09 +0100

    Versie ts

---
+++ src/firebase.ts
@@ -0,0 +1,47 @@
+import { initializeApp } from 'firebase/app';
+import { useCollection } from 'vuefire'
+import { getAuth, signInAnonymously } from "firebase/auth";
+import { setDoc, deleteDoc, doc, getDoc,getFirestore, query,collection, orderBy, addDoc }  from 'firebase/firestore';
+import { ref, computed } from 'vue';
+
+
+const app = initializeApp({
+  apiKey: "AIzaSyAvLVezzPLDjG8UKkQejIiCkZATaXG2uIs",
+authDomain: "quizz-127ca.firebaseapp.com",
+projectId: "quizz-127ca",
+storageBucket: "quizz-127ca.appspot.com",
+messagingSenderId: "956457625466",
+appId: "1:956457625466:web:20cfc8a5dfeeee16cb18c3"
+})//lite.getFirestore(app);
+
+const auth = getAuth(app);
+signInAnonymously(auth)
+  .then(() => {
+    // Signed in..
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ...
+  });
+export const db = getFirestore(app)
+
+export const colQuestions = useCollection(query(collection(db, 'questions'),orderBy('nr')))
+export const colUsers = useCollection(query(collection(db, 'users'),orderBy('score','desc')))
+export const selectedQuestion =computed(() =>colQuestions.value.filter(q=>q.selected)[0] )
+export const authenticated = ref(false)
+export const isHost = ref(false)
+export const user = ref()
+
+
+export async  function saveDoc(c:string,item:any){
+  if (item.id){
+      return await setDoc(doc(collection(db, c), item.id),item)
+  }else{
+    return await addDoc(collection(db, c),item)
+  }
+}
+
+export async function deleteItem(c:string,item:any){
+  await deleteDoc(doc(db, c, item.id));
+}
